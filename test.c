d47b67ef700086099878a4bac5b0f4a0602fe321
Moritz Warning <moritzwarning@web.de>
2018-08-30 09:21:01 +0200

    code style

--- test.c
+++ test.c
@@ -19,12 +19,14 @@
 
 
 // Helper to convert binary to hex
-static char *bytes_to_hex( const uint8_t bin[], size_t len ) {
+static char *bytes_to_hex(const uint8_t bin[], size_t len)
+{
     static const char hexchars[16] = "0123456789abcdef";
     static char hex[512];
     size_t i;
 
-    for( i = 0; i < len; ++i ) {
+    for (i = 0; i < len; ++i)
+    {
         hex[2 * i] = hexchars[bin[i] / 16];
         hex[2 * i + 1] = hexchars[bin[i] % 16];
     }
@@ -33,18 +35,18 @@ static char *bytes_to_hex( const uint8_t bin[], size_t len ) {
 }
 
 // Helper to print public keys
-static void dump_pubkey( const char *title, mbedtls_ecdsa_context *key ) {
+static void dump_pubkey(const char *title, mbedtls_ecdsa_context *key)
+{
     uint8_t buf[512];
     size_t len;
 
-    if( mbedtls_ecp_point_write_binary( &key->grp, &key->Q,
-            MBEDTLS_ECP_PF_UNCOMPRESSED, &len, buf, sizeof(buf) ) != 0 )
-    {
+    if (mbedtls_ecp_point_write_binary(&key->grp, &key->Q,
+            MBEDTLS_ECP_PF_UNCOMPRESSED, &len, buf, sizeof(buf)) != 0) {
         printf("internal error\n");
         return;
     }
 
-    printf("%s %s (%d bits)\n", title, bytes_to_hex( buf, len ), (int) key->grp.pbits);
+    printf("%s %s (%d bits)\n", title, bytes_to_hex(buf, len), (int) key->grp.pbits);
 }
 
 #if 0
@@ -52,7 +54,7 @@ static void dump_pubkey( const char *title, mbedtls_ecdsa_context *key ) {
 static void print_mpi(const char *title, const mbedtls_mpi *n) {
     char buf[512];
     size_t olen = 0;
-    if(mbedtls_mpi_write_string( n, 16, buf, sizeof(buf), &olen ) != 0) {
+    if (mbedtls_mpi_write_string(n, 16, buf, sizeof(buf), &olen) != 0) {
         printf("print_mpi error\n");
         exit(1);
     }
@@ -64,14 +66,14 @@ static void print_mpi(const char *title, const mbedtls_mpi *n) {
 static void check_prime(mbedtls_mpi *P){
     mbedtls_mpi tmp;
     mbedtls_mpi _4;
-    mbedtls_mpi_init( &tmp );
-    mbedtls_mpi_init( &_4 );
+    mbedtls_mpi_init(&tmp);
+    mbedtls_mpi_init(&_4);
 
-    mbedtls_mpi_lset( &_4, 4 );
-    mbedtls_mpi_copy( &tmp, P );
+    mbedtls_mpi_lset(&_4, 4);
+    mbedtls_mpi_copy(&tmp, P);
 
-    mbedtls_mpi_mod_mpi( &tmp, &tmp, &_4 );
-    print_mpi( "We can use fast sqrt mod P if the output is 3: ", &tmp );
+    mbedtls_mpi_mod_mpi(&tmp, &tmp, &_4);
+    print_mpi("We can use fast sqrt mod P if the output is 3: ", &tmp);
 }
 #endif
 
@@ -79,7 +81,8 @@ static void check_prime(mbedtls_mpi *P){
 * Test mbedtls_ecp_compress() / mbedtls_ecp_decompress() by signing a hash and
 * transfering the public key in its compressed form from ctx_sign to ctx_verify.
 */
-int test( int ecparams ) {
+int test(int ecparams)
+{
     int ret;
     mbedtls_pk_context ctx_verify;
     mbedtls_pk_context ctx_sign;
@@ -90,119 +93,119 @@ int test( int ecparams ) {
     size_t sig_len;
     const char *pers = "ecdsa";
 
-    mbedtls_pk_init( &ctx_sign );
-    mbedtls_pk_init( &ctx_verify );
-    mbedtls_ctr_drbg_init( &ctr_drbg );
+    mbedtls_pk_init(&ctx_sign);
+    mbedtls_pk_init(&ctx_verify);
+    mbedtls_ctr_drbg_init(&ctr_drbg);
 
-    printf( "Seeding the random number generator...\n" );
-    mbedtls_entropy_init( &entropy );
+    printf("Seeding the random number generator...\n");
+    mbedtls_entropy_init(&entropy);
 
-    if( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, mbedtls_entropy_func, &entropy,
-        (const unsigned char *) pers, strlen( pers ) ) ) != 0 )
+    if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+        (const unsigned char *) pers, strlen(pers))) != 0)
     {
-        printf( "mbedtls_ctr_drbg_seed returned %d\n", ret );
-        return( 1 );
+        printf("mbedtls_ctr_drbg_seed returned %d\n", ret);
+        return(1);
     }
 
-    printf( "Generating key pair...\n" );
+    printf("Generating key pair...\n");
 
-    if( ( ret = mbedtls_pk_setup( &ctx_sign, mbedtls_pk_info_from_type( MBEDTLS_PK_ECKEY ) ) ) != 0 )
+    if ((ret = mbedtls_pk_setup(&ctx_sign, mbedtls_pk_info_from_type(MBEDTLS_PK_ECKEY))) != 0)
     {
-        printf( "mbedtls_pk_setup returned -0x%04x", -ret );
-        return( 1 );
+        printf("mbedtls_pk_setup returned -0x%04x", -ret);
+        return(1);
     }
 
-    if( (ret = mbedtls_ecp_gen_key( ecparams, mbedtls_pk_ec( ctx_sign ),
-        mbedtls_ctr_drbg_random, &ctr_drbg ) ) != 0 )
+    if ((ret = mbedtls_ecp_gen_key(ecparams, mbedtls_pk_ec(ctx_sign),
+        mbedtls_ctr_drbg_random, &ctr_drbg)) != 0)
     {
-        printf( "mbedtls_ecp_gen_key returned %d\n", ret );
-        return( 1 );
+        printf("mbedtls_ecp_gen_key returned %d\n", ret);
+        return(1);
     }
  
-    dump_pubkey( "Public key: ", mbedtls_pk_ec( ctx_sign ) );
+    dump_pubkey("Public key: ", mbedtls_pk_ec(ctx_sign));
 
-    printf( "Signing message...\n" );
+    printf("Signing message...\n");
 
-    if( ( ret = mbedtls_ecdsa_write_signature( mbedtls_pk_ec( ctx_sign ), MBEDTLS_MD_SHA256,
-            hash, sizeof( hash ), sig, &sig_len, mbedtls_ctr_drbg_random, &ctr_drbg ) ) != 0 )
+    if ((ret = mbedtls_ecdsa_write_signature(mbedtls_pk_ec(ctx_sign), MBEDTLS_MD_SHA256,
+            hash, sizeof(hash), sig, &sig_len, mbedtls_ctr_drbg_random, &ctr_drbg)) != 0)
     {
-        printf( "mbedtls_ecdsa_genkey returned %d\n", ret );
-        return( 1 );
+        printf("mbedtls_ecdsa_genkey returned %d\n", ret);
+        return(1);
     }
 
-    printf( "ok (signature length = %lu)\n", sig_len );
-    printf( " + Hash: %s\n", bytes_to_hex(hash, strlen((char*) hash)));
-    printf( " + Signature: %s\n", bytes_to_hex(sig, sig_len));
+    printf("ok (signature length = %lu)\n", sig_len);
+    printf(" + Hash: %s\n", bytes_to_hex(hash, strlen((char*) hash)));
+    printf(" + Signature: %s\n", bytes_to_hex(sig, sig_len));
 
-    printf( "Preparing verification context...\n" );
+    printf("Preparing verification context...\n");
 
     unsigned char buf[300];
     size_t buflen = 0;
 
-    if( ( ret = mbedtls_pk_setup( &ctx_verify, mbedtls_pk_info_from_type( MBEDTLS_PK_ECKEY ) ) ) != 0 )
+    if ((ret = mbedtls_pk_setup(&ctx_verify, mbedtls_pk_info_from_type(MBEDTLS_PK_ECKEY))) != 0)
     {
-        printf( "mbedtls_pk_setup returned -0x%04x", -ret );
-        return( 1 );
+        printf("mbedtls_pk_setup returned -0x%04x", -ret);
+        return(1);
     }
 
-    mbedtls_ecp_group_load( &mbedtls_pk_ec( ctx_verify )->grp, ecparams );
+    mbedtls_ecp_group_load(&mbedtls_pk_ec(ctx_verify)->grp, ecparams);
 
     // MBEDTLS_ECP_PF_COMPRESSED is supported here!
-    if( ( ret = mbedtls_ecp_point_write_binary( &mbedtls_pk_ec( ctx_sign )->grp,
-            &mbedtls_pk_ec( ctx_sign )->Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &buflen,
-            buf, sizeof(buf) )) != 0 )
+    if ((ret = mbedtls_ecp_point_write_binary(&mbedtls_pk_ec(ctx_sign)->grp,
+            &mbedtls_pk_ec(ctx_sign)->Q, MBEDTLS_ECP_PF_UNCOMPRESSED, &buflen,
+            buf, sizeof(buf))) != 0)
     {
-        printf( "mbedtls_ecp_point_write_binary returned: %d\n", ret );
-        return( 1 );
+        printf("mbedtls_ecp_point_write_binary returned: %d\n", ret);
+        return(1);
     }
 
     {
-        printf( "Perform key transformations...\n" );
+        printf("Perform key transformations...\n");
 
         size_t compressed_len;
         unsigned char compressed[512];
 
         // We have the uncompressed key
-        printf( "starting point:         %s\n", bytes_to_hex(buf, buflen) );
+        printf("starting point:         %s\n", bytes_to_hex(buf, buflen));
 
         // compress key from buf to compressed
         ret = mbedtls_ecp_compress(&mbedtls_pk_ec(ctx_verify)->grp, buf, buflen,
             compressed, &compressed_len, sizeof(compressed));
-        printf( "mbedtls_ecp_compress:   %s\n", bytes_to_hex(compressed, compressed_len) );
+        printf("mbedtls_ecp_compress:   %s\n", bytes_to_hex(compressed, compressed_len));
 
         // decompress key from compressed back into buf
         memset(buf, 0, sizeof(buf)); // Make sure we don't cheat :)
         ret = mbedtls_ecp_decompress(&mbedtls_pk_ec(ctx_verify)->grp, compressed, compressed_len,
             buf, &buflen, sizeof(buf));
-        printf( "mbedtls_ecp_decompress: %s\n", bytes_to_hex(buf, buflen) );
+        printf("mbedtls_ecp_decompress: %s\n", bytes_to_hex(buf, buflen));
     }
 
     // MBEDTLS_ECP_PF_COMPRESSED format is _not_ supported here!
-    if( (ret = mbedtls_ecp_point_read_binary( &mbedtls_pk_ec( ctx_verify)->grp,
-            &mbedtls_pk_ec(ctx_verify)->Q, buf, buflen )) != 0 )
+    if ((ret = mbedtls_ecp_point_read_binary(&mbedtls_pk_ec(ctx_verify)->grp,
+            &mbedtls_pk_ec(ctx_verify)->Q, buf, buflen)) != 0)
     {
-        printf( "mbedtls_ecp_point_read_binary returned: %d\n", ret );
+        printf("mbedtls_ecp_point_read_binary returned: %d\n", ret);
         exit(1);
     }
 
-    printf( "Verifying signature...\n" );
+    printf("Verifying signature...\n");
 
-    if( (ret = mbedtls_ecdsa_read_signature( mbedtls_pk_ec(ctx_verify),
-            hash, sizeof( hash ), sig, sig_len )) != 0 )
+    if ((ret = mbedtls_ecdsa_read_signature(mbedtls_pk_ec(ctx_verify),
+            hash, sizeof(hash), sig, sig_len)) != 0)
     {
-        printf( "mbedtls_ecdsa_read_signature returned %d\n", ret );
+        printf("mbedtls_ecdsa_read_signature returned %d\n", ret);
         exit(1);
     }
     else
     {
-        printf( "Signature is valid!\n" );
+        printf("Signature is valid!\n");
     }
 
-    mbedtls_pk_free( &ctx_verify );
-    mbedtls_pk_free( &ctx_sign );
+    mbedtls_pk_free(&ctx_verify);
+    mbedtls_pk_free(&ctx_sign);
 
-    mbedtls_ctr_drbg_free( &ctr_drbg );
-    mbedtls_entropy_free( &entropy );
+    mbedtls_ctr_drbg_free(&ctr_drbg);
+    mbedtls_entropy_free(&entropy);
 
     return 0;
 }
@@ -213,5 +216,5 @@ int main(int argc, char **argv) {
     * This implementation only works for curves where curve->p == 3 (mod 4) holds,
     * such as NIST / Brainpool / "Koblitz" curves used in mbedtls.
     */
-    return test( MBEDTLS_ECP_DP_SECP256R1 );
+    return test(MBEDTLS_ECP_DP_SECP256R1);
 }
